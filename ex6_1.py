06f812853f65994ba297350d707181b06df41182
rlarudgus99 <rhkdwldnjsch@naver.com>
2022-09-16 15:11:03 +0900

    '2022-09-15'

--- ex6_1.py
+++ ex6_1.py
@@ -12,6 +12,9 @@
 
 # print(array)
 
+
+
+
 array = [7,5,9,0,3,1,6,2,4,8]
 
 for i in range(len(array)):
@@ -20,4 +23,6 @@ for i in range(len(array)):
         if array[min_index] > array[j]:
             min_index = j
     array[i], array[min_index] = array[min_index], array[i]
+
 print(array)
+
